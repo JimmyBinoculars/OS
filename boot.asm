ba7a0070202d0560bda63c50f504e358e0410a8f
JimmyBinoculars <91781252+JimmyBinoculars@users.noreply.github.com>
2024-04-27 11:15:42 +0100

    Update stuff

--- boot.asm
+++ boot.asm
@@ -1,37 +1,31 @@
-; Bootloader
+; bootloader.asm
 
-;Define origin
 ORG 0x7C00
 
-;Entry point
 start:
-    ;Clear interrupts
     cli
 
-    ;Setup data segments
     xor ax, ax
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
 
-    ;Setup stack
     mov ss, ax
     mov sp, 0x7C00
 
-    ;Load kernel
-    mov bx, 0x8000
-    mov dh, 15  ; Number of sectors to load
-    mov dl, 0x80 ; Drive number (0x80 for first hard disk)
-    mov ah, 0x02 ; Read disk sectors function
-    mov al, dh   ; Number of sectors to read
-    mov ch, 0x00 ; Cylinder number
-    mov cl, 0x02 ; Sector number
-    int 0x13     ; BIOS interrupt
+    ; Load the kernel into memory
+    mov bx, 0x8000      ; Load address in memory where the kernel will be loaded
+    mov dh, 15          ; Number of sectors to load
+    mov dl, 0x80        ; Drive number (0x80 for first hard disk)
+    mov ah, 0x02        ; Read disk sectors function
+    mov al, dh          ; Number of sectors to read
+    mov ch, 0x00        ; Cylinder number
+    mov cl, 0x02        ; Sector number
+    int 0x13            ; BIOS interrupt
 
-    ;Jump to kernel
+    ; Jump to the kernel
     jmp 0x8000
 
-;Fill remaning bytes to reach 512
 times 510 - ($ - start) db 0
-dw 0xAA55 ; Boot signature
+dw 0xAA55
