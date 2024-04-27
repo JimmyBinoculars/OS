786ff5c5d69e78bcffe0dc0d0e7d3cae532b4f79
JimmyBinoculars <91781252+JimmyBinoculars@users.noreply.github.com>
2024-04-27 11:47:05 +0100

    Started tutorial

--- bootloader.asm
+++ bootloader.asm
@@ -1,31 +1,43 @@
-; bootloader.asm
+[bits 16]
+[org 0x7c00]
 
-ORG 0x7C00
+; where to load the kernel to
+KERNEL_OFFSET equ 0x1000
 
-start:
-    cli
+; BIOS sets boot drive in 'dl'; store for later use
+mov [BOOT_DRIVE], dl
 
-    xor ax, ax
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
+; setup stack
+mov bp, 0x9000
+mov sp, bp
 
-    mov ss, ax
-    mov sp, 0x7C00
+call load_kernel
+call switch_to_32bit
 
-    ; Load the kernel into memory
-    mov bx, 0x8000      ; Load address in memory where the kernel will be loaded
-    mov dh, 15          ; Number of sectors to load
-    mov dl, 0x80        ; Drive number (0x80 for first hard disk)
-    mov ah, 0x02        ; Read disk sectors function
-    mov al, dh          ; Number of sectors to read
-    mov ch, 0x00        ; Cylinder number
-    mov cl, 0x02        ; Sector number
-    int 0x13            ; BIOS interrupt
+jmp $
 
-    ; Jump to the kernel
-    jmp 0x8000
+%include "disk.asm"
+%include "gdt.asm"
+%include "switch-to-32bit.asm"
 
-times 510 - ($ - start) db 0
-dw 0xAA55
+[bits 16]
+load_kernel:
+    mov bx, KERNEL_OFFSET ; bx -> destination
+    mov dh, 2             ; dh -> num sectors
+    mov dl, [BOOT_DRIVE]  ; dl -> disk
+    call disk_load
+    ret
+
+[bits 32]
+BEGIN_32BIT:
+    call KERNEL_OFFSET ; give control to the kernel
+    jmp $ ; loop in case kernel returns
+
+; boot drive variable
+BOOT_DRIVE db 0
+
+; padding
+times 510 - ($-$$) db 0
+
+; magic number
+dw 0xaa55
