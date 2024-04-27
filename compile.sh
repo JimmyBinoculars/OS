429ffa092a68e7cffdf27c313f8741bd573ad90d
JimmyBinoculars <91781252+JimmyBinoculars@users.noreply.github.com>
2024-04-27 14:41:51 +0100

    Testing

--- compile.sh
+++ compile.sh
@@ -1,5 +1,9 @@
-# Compile the bootloader
+# Compile both parts of the bootloader
 nasm -f bin bootloader.asm -o bootloader.bin
+nasm -f bin videoloader.asm -o videoloader.bin
+
+# Combine both parts into the final bootloader binary
+cat bootloader.bin videoloader.bin > bootloader_combined.bin
 
 # Compile the kernel
 gcc -ffreestanding -c kernel.c -o kernel.o
@@ -7,5 +11,5 @@ ld -o kernel.bin -Ttext 0x1000 --oformat binary kernel.o
 
 # Combine bootloader and kernel binaries into disk image
 dd if=/dev/zero of=disk.img bs=512 count=2880  # Create an empty disk image (1.44MB)
-dd if=bootloader.bin of=disk.img bs=512 count=1 conv=notrunc  # Write bootloader to first sector
+dd if=bootloader_combined.bin of=disk.img bs=512 count=1 conv=notrunc  # Write bootloader to first sector
 dd if=kernel.bin of=disk.img bs=512 seek=2 conv=notrunc  # Write kernel to third sector
