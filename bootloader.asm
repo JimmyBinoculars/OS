01c3b22ccd140cb4d3f43c53dc461def99198f74
JimmyBinoculars <91781252+JimmyBinoculars@users.noreply.github.com>
2024-04-27 11:59:24 +0100

    Gone back to previous

--- bootloader.asm
+++ bootloader.asm
@@ -1,25 +1,22 @@
-bits 16
+BITS 16
 
-; Tell NASM that we expect our bootloader to be laoded at this address, hence offsets should be calculated in relation to this address
-org 0x7c00
+start:
+    ; Set up segment registers
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
 
-; Define a label X that is a memory offset of the start of our code.
-; It points to a character B.
-x:
-    db "B"
+    ; Load kernel into memory
+    mov bx, KERNEL_OFFSET    ; Memory address where kernel will be loaded
+    mov ah, 02h              ; BIOS read sector function
+    mov al, 1                ; Read one sector
+    mov ch, 0                ; Cylinder number
+    mov cl, 2                ; Sector number
+    mov dh, 0                ; Head number
+    mov dl, 0                ; Drive number (usually floppy disk or hard disk)
+    int 13h                  ; Call BIOS interrupt for disk I/O
 
-; Move offset of x to bx
-mov bx, x
+    ; Jump to the kernel
+    jmp KERNEL_OFFSET:0
 
-; Add 0x7c00 to bx - it's universally known that BIOS loads bootloaders to this location.
-; add bx, 0x7c00
-
-; Move contents of bx to al
-mov al, [bx]
-
-; Prepare interrupt to print a character in TTY mode and issue the interrupt
-mov ah, 0x0e
-int 0x10
-
-times 510 - ($-$$) db 0
-dw 0xaa55
+KERNEL_OFFSET equ 0x1000    ; Adjust this according to where you want to load the kernel
