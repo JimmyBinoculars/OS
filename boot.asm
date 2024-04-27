805e6f3879bc01ef25f806e828d0e47d6e20cd3e
JimmyBinoculars <91781252+JimmyBinoculars@users.noreply.github.com>
2024-04-27 19:14:13 +0100

    EP. 4

--- boot.asm
+++ boot.asm
@@ -1,21 +1,6 @@
 [org 0x7c00]
-mov ah, 0x0e
-mov bx, variableName
-
-printString:
-    mov al, [bx]
-    cmp al, 0
-    je end
-    int 0x10
-    inc bx
-    jmp printString
-
-end:
-
+mov
 jmp $
-
-variableName:
-    db "Hello world!", 0
 
 times 510-($-$$) db 0
 db 0x55, 0xaa
