95d758a6a737835c4415edca3bdb111e49d32693
JimmyBinoculars <91781252+JimmyBinoculars@users.noreply.github.com>
2024-04-27 19:56:13 +0100

    Chapter 6 (32bit)

--- boot.asm
+++ boot.asm
@@ -1,21 +1,56 @@
 [org 0x7c00]
-mov [diskNum], dl
 
-mov bx, 0x7e00
+mov [BOOT_DISK], dl
 
-mov ah, 2
-mov al, 1
-mov ch, 0
-mov dh, 0
-mov cl, 2
-mov dl, [diskNum]
-int 0x13
+CODE_SEG equ GDT_code - GDT_start
+DATA_SEG equ GDT_data - GDT_start
 
-mov ah, 0x0e
-mov al, [0x7e00]
-int 0x10
+cli
+lgdt [GDT_descriptor]
+mov eax, cr0
+or eax, 1
+mov cr0, eax
+jmp CODE_SEG:start_protected_mode
+
 jmp $
-diskNum: db 0
+
+
+GDT_start:                          ; must be at the end of real mode code
+    GDT_null:
+        dd 0x0
+        dd 0x0
+
+    GDT_code:
+        dw 0xffff
+        dw 0x0
+        db 0x0
+        db 0b10011010
+        db 0b11001111
+        db 0x0
+
+    GDT_data:
+        dw 0xffff
+        dw 0x0
+        db 0x0
+        db 0b10010010
+        db 0b11001111
+        db 0x0
+
+GDT_end:
+
+GDT_descriptor:
+    dw GDT_end - GDT_start - 1
+    dd GDT_start
+
+
+[bits 32]
+start_protected_mode:
+    mov al, 'A'
+    mov ah, 5
+    mov [0xb8000], ax
+    jmp $
+
+BOOT_DISK: db 0
+
 times 510-($-$$) db 0
-db 0x55, 0xaa
-times 512 db 'A'
+dw 0xaa55
