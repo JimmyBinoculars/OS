6dcf46d7c2cc77bd798e9e363e0d98a01049bd2b
JimmyBinoculars <91781252+JimmyBinoculars@users.noreply.github.com>
2024-04-28 08:57:47 +0100

    Hello world

--- kernel.cpp
+++ kernel.cpp
@@ -1,4 +1,11 @@
 extern "C" void main() {
+    char *message = "Hello World!";
+    char *vga_buffer = (char*)0xb8000;
+
+    for (int i=0; message[i] !='\0'; ++i) {
+        vga_buffer[i * 2] = message[i];
+        vga_buffer[i * 2 + 1] = 0x0F;
+    }
     *(char*)0xb8000 = 'H';
     return;
 }
