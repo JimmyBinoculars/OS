e05e7feee49ce7f0e232ff66e6c3bbdd8263c0d7
JimmyBinoculars <91781252+JimmyBinoculars@users.noreply.github.com>
2025-03-18 21:02:48 +0000

    Restarting

--- compile.sh
+++
@@ -1,7 +0,0 @@
-i386-elf-gcc -ffreestanding -m32 -g -c "kernel.cpp" -o "kernel.o"
-nasm "kernel_entry.asm" -f elf -o "kernel_entry.o"
-i386-elf-ld -o "full_kernel.bin" -Ttext 0x1000 "kernel_entry.o" "kernel.o" --oformat binary
-nasm "boot.asm" -f bin -o "boot.bin"
-cat "boot.bin" "full_kernel.bin" > "everything.bin"
-nasm zeros.asm -f bin -o "zeros.bin"
-cat "everything.bin" "zeros.bin" > "OS.bin"
